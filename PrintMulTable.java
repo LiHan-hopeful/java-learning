f44d97033d661f23dcc20cc15293a573b80c75f7
LiHan-hopeful <1036177761@qq.com>
2019-12-29 23:20:34 +0800

    打印乘法表

---
+++ PrintMulTable.java
@@ -0,0 +1,11 @@
+public class PrintMulTable{
+    public static void main(String[] args) {
+        System.out.println("打印乘法表：");
+        for(int i = 1;i <= 9;i++){
+            for(int j = 1;j <= i;j++){
+                System.out.print(j+"*"+i+"="+i*j+"   ");
+            }
+            System.out.println("\n");
+        }
+    }
+}
